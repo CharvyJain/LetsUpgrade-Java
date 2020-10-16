b987b0980a086552215afd4e633105b003be082f
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2020-10-16 17:18:52 +0530

    Create Main.java

---
+++ DAY - 3/Employee/Main.java
@@ -0,0 +1,12 @@
+package com.day3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee e = new Employee();
+        e.Details();
+        e.display();
+
+    }
+}
