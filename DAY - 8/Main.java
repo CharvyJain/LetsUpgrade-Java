9cb8e0c19785c23763042f5e128c50a589836f90
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2020-11-01 18:44:20 +0530

    Create Main.java

---
+++ DAY - 8/Main.java
@@ -0,0 +1,58 @@
+// ANSWER OF Q3 AND Q4
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Doctor[] doctors = new Doctor[3];
+        Engineer[] engineers = new Engineer[3];
+        Pilot[] pilots = new Pilot[3];
+
+        for (int i=0; i<3; i++){
+            doctors[i] = new Doctor();
+            engineers[i] = new Engineer();
+            pilots[i] = new Pilot();
+        }
+
+        //DOCTOR
+        for (int i=0; i<3; i++){
+            doctors[i].getDetails();
+            doctors[i].getdetailsford();
+        }
+
+                
+        //ENGINNER
+        for (int i=0; i<3; i++){
+            engineers[i].getDetails();
+            engineers[i].getdetailsfore();
+        }
+
+
+        //PILOT
+ 
+        for (int i=0; i<3; i++){
+            pilots[i].getDetails();
+            pilots[i].getdetailsforp();
+        }
+
+        //Printing Doctors Details
+        for (int i=0; i<3; i++){
+            doctors[i].displayDetails();
+            doctors[i].displaydetailsford();
+        } 
+        
+        //Printing Engineers Details
+        for (int i=0; i<3; i++){
+            engineers[i].displayDetails();
+            engineers[i].displaydetailsfore();
+        }
+
+        //Printing Pilots Details
+        for (int i=0; i<3; i++){
+            pilots[i].displayDetails();
+            pilots[i].displaydetailsforp();
+        }
+
+    }
+}
