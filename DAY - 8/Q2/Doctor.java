f6e176d393e976c8de2a687b810ccc7c4b8718e2
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2020-11-01 18:15:35 +0530

    Update Doctor.java

--- DAY - 8/Q2/Doctor.java
+++ DAY - 8/Q2/Doctor.java
@@ -9,15 +9,15 @@ public class Doctor extends Employee {
 
     public void getdetailsford() {
 	System.out.println("Enter Hospital Name: ");
-	this.hospitalName = sc.next();
+	hospitalName = sc.next();
       	System.out.println("Enter Years of Experience: ");
-	this.yearsOfExperience = sc.nextInt();
+	yearsOfExperience = sc.nextInt();
 	System.out.println("Enter your specific field: ");
-	this.specialist = sc.next();
+	specialist = sc.next();
     }
     public void displaydetailsford() {
-	System.out.println("Hospital Name: " + this.hospitalName);
-	System.out.println("Years of Experience: " + this.yearsOfExperience);
-	System.out.println("Specialist in: " + this.specialist);
+	System.out.println("Hospital Name: " + hospitalName);
+	System.out.println("Years of Experience: " + yearsOfExperience);
+	System.out.println("Specialist in: " + specialist);
 		}
 }
